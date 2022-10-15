f83f477caa23b7b1f5929642c863e4ecbea98fdc
Adam Lee <adam@codelabx.ltd>
2022-10-15 14:09:34 +0100

    Register & boot extension service providers

--- src/XtendLaravelServiceProvider.php
+++ src/XtendLaravelServiceProvider.php
@@ -2,12 +2,59 @@
 
 namespace CodeLabX\XtendLaravel;
 
+use CodeLabX\XtendLaravel\Base\ExtendsProvider;
 use CodeLabX\XtendLaravel\Commands\XtendLaravelSetupCommand;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Symfony\Component\Finder\SplFileInfo;
 
 class XtendLaravelServiceProvider extends PackageServiceProvider
 {
+    protected Collection $extendedPackageFacades;
+
+    public function registeringPackage()
+    {
+        $this->setViewsPath();
+        $this->registerPackageProviders();
+        $this->registerWithPackageFacades();
+    }
+
+    protected function registerPackageProviders(): void
+    {
+        $this->getPackageProviders()->each(function ($provider) {
+            $this->app->register($provider);
+        });
+    }
+
+    protected function getPackageProviders(): Collection
+    {
+        $this->extendedPackageFacades = collect();
+
+        $xtendPackageDir = 'xtend';
+
+        return collect(app(Filesystem::class)->allFiles(app()->basePath($xtendPackageDir)))
+            ->filter(fn (SplFileInfo $file) => Str::endsWith($file->getRelativePath(), 'Providers'))
+            ->each(function (SplFileInfo $file) {
+                $packageNamespace = Str::of($file->getRelativePath())->between('Extensions/', '/Providers')->toString();
+                $this->extendedPackageFacades->put($packageNamespace, 'Xtend\\Extensions\\'.$packageNamespace.'\\Facades\\'.$packageNamespace.'Extend');
+            })
+            ->map(function (SplFileInfo $file): string {
+                return (string) Str::of('Xtend')
+                    ->append('\\', $file->getRelativePathname())
+                    ->replace(['/', '.php'], ['\\', '']);
+            });
+    }
+
+    protected function registerWithPackageFacades(): void
+    {
+        $this->extendedPackageFacades->each(function (ExtendsProvider|string $extendedProvider) {
+            $extendedProvider::withRegister();
+        });
+    }
+
     public function configurePackage(Package $package): void
     {
         /*
@@ -23,5 +70,23 @@ class XtendLaravelServiceProvider extends PackageServiceProvider
             ->hasCommands([
                 XtendLaravelSetupCommand::class,
             ]);
+    }
+
+    public function bootingPackage()
+    {
+        $this->bootWithPackageFacades();
+        $this->loadViewsFrom($this->package->basePath('/../resources/views'), 'xtend-laravel');
+    }
+
+    protected function bootWithPackageFacades(): void
+    {
+        $this->extendedPackageFacades->each(function (ExtendsProvider|string $extendedProvider) {
+            $extendedProvider::withBoot();
+        });
+    }
+
+    protected function setViewsPath(): void
+    {
+        config()->set('view.paths', array_merge([__DIR__.'/../resources/views'], config('view.paths')));
     }
 }
