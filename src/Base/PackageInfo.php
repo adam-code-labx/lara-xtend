1cc74a90d0c91909e891c7f86a114ccebca5c09b
Adam Lee <adam@codelabx.ltd>
2022-10-20 16:36:07 +0100

    Package info + install default xtend packages

---
+++ src/Base/PackageInfo.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace CodeLabX\XtendLaravel\Base;
+
+use Composer\InstalledVersions;
+
+class PackageInfo
+{
+    protected string $version = '*';
+
+    protected array $data = [];
+
+    protected string $namespace = 'CodeLabX\\XtendLaravel\\Extensions';
+
+    protected bool $isEnabled = false;
+
+    public function __construct(protected string $name) {
+        $this->setVersion();
+    }
+
+    public static function make(string $name): static
+    {
+        return resolve(static::class, ['name' => $name]);
+    }
+
+    public function setVersion(): static
+    {
+        $this->version = InstalledVersions::getVersion($this->name);
+        return $this;
+    }
+
+    public function data(array $data): static
+    {
+        $this->data = $data;
+        return $this;
+    }
+
+    public function namespace(string $namespace): static
+    {
+        $this->namespace = $namespace;
+        return $this;
+    }
+
+    public function enabled(bool $isEnabled = true): static
+    {
+        $this->isEnabled = $isEnabled;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'version' => $this->version,
+            'data' => $this->data,
+            'namespace' => $this->namespace,
+            'is_enabled' => $this->isEnabled,
+        ];
+    }
+}
--- src/Base/XtendPackageManager.php
+++ src/Base/XtendPackageManager.php
@@ -5,4 +5,22 @@ namespace CodeLabX\XtendLaravel\Base;
 class XtendPackageManager
 {
     public function __construct(protected XtendPackage $xtend) {}
+
+    public function installPackages(): void
+    {
+        foreach ($this->getPackages() as $package) {
+            $this->xtend->updateOrCreate(
+                ['name' => $package->getName()],
+                $package->toArray()
+            );
+        }
+    }
+
+    protected function getPackages(): array
+    {
+        return [
+            PackageInfo::make('laravel/framework')->enabled(),
+            PackageInfo::make('livewire/livewire')->enabled(),
+        ];
+    }
 }
