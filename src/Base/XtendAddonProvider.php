d39dab95852d761775ed83f256364006a5d6007e
Adam Lee <adam@codelabx.ltd>
2023-03-19 02:55:45 +0000

    XtendAddon provider

---
+++ src/Base/XtendAddonProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CodeLabX\XtendLaravel\Base;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class XtendAddonProvider
+ */
+abstract class XtendAddonProvider extends ServiceProvider
+{
+}
